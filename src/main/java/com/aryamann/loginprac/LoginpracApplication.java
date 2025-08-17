a1400e09e2218ad23ee3e9d6a4c7cfc6ea03e6c4
Aryamann Singh <aryamann.singh21@gmail.com>
2025-08-17 15:56:40 +0530

    first commit

---
+++ src/main/java/com/aryamann/loginprac/LoginpracApplication.java
@@ -0,0 +1,13 @@
+package com.aryamann.loginprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginpracApplication.class, args);
+	}
+
+}
